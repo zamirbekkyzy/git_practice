e7a6e61c5fa86fe3f744c0347f1cfb439478b267
gulnur <zamirbekkyzygulnur00@gmail.com>
2023-08-02 14:03:14 +0600

    initial commit

---
+++ git_commands.py
@@ -0,0 +1,11 @@
+git init - .git
+git remote add name url
+git pull origin master
+git status 
+git add name file
+git commit -m "comment"
+git branch
+git branch name branch
+git checkout name branch
+git push origin master
+git reset filename
